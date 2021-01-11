4268a1f968aa55b59cbc8f7be2f044c6e68bd449
Rohan-J-S <53927306+Rohan-J-S@users.noreply.github.com>
2021-01-11 09:13:21 +0530

    Add files via upload

--- first_code_2021.py
+++ first_code_2021.py
@@ -1,23 +1,38 @@
 
 def check(inp_letter,word):
+    if inp_letter == word:
+        return True
 
-        if inp_letter in word:
-            return word.count(inp_letter)
+    elif inp_letter in word:
+        return word.count(inp_letter)+1
 
-        else:
-            return False
+    else:
+        return False
+
+print("welcome to hangman.  ............ . enter guess when you think you are ready to guess the word")
 
 terminate = False
 tries = 7
 letters_left = 0
+entered_letters = set()
+target = 4
 
 while tries:
     inp_letter = input("enter a single letter: ")
+    
 
-    if len(inp_letter)>1:
+    if inp_letter == "guess":
+        inp_letter = input("enter the word (or a letter if you want to): ")
+
+    elif len(inp_letter)>1:
         print("you entered an invalid input ")
         continue
 
+    elif inp_letter in entered_letters:
+        print("you have aldready entered that letter: ")
+        continue
+
+    entered_letters.add(inp_letter)
     a = check(inp_letter,"test")
     print(a)
     
@@ -25,11 +40,12 @@ while tries:
         tries -= 1
         print(tries, "tries left")
 
-    elif a:
+    elif a-1:
         letters_left += check(inp_letter,"test")
         print(letters_left)
+        target += 1
 
-    if letters_left == 4:
+    elif letters_left == target or a == True:
         print("you got the word ")
         break
 else:
