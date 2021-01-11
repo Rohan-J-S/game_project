0f308335939fb1d6d4afe96eef5a6b6068226018
Rohan-J-S <53927306+Rohan-J-S@users.noreply.github.com>
2021-01-11 12:06:01 +0530

    Add files via upload

--- encrypter.py
+++ encrypter.py
@@ -1,4 +1,5 @@
-
+x = open("movie_list.txt",'r')
+text = x.readlines()
 def encrypt(text):
 
     key = {'a':'h','b':'d','c':'s','d':'n','e':'a','f':'i','g':'q','h':'k','i':'e','j':'y','k':'u','l':'j','m':'o','n':'t','o':'c','p':'w','q':'m','r':'b','s':'r','t':'f','u':'l','v':'x','w':'p','x':'g','y':'z','z':'v',' ':' '}
@@ -14,5 +15,15 @@ def encrypt(text):
             s += key[text[y][z]]
         text[y] = s
     return text
+a = encrypt(text)
+b = encrypt(a)
+
+x.close()
+y = open("movie_list.txt","w")
+for z in b:
+    
+    y.write (z+"\n")
+y.close()
+
 
 
