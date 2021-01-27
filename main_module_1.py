5d9fbb811f45542f3511066fe6f5ccdd2de78a1e
Persay1981 <75056681+Persay1981@users.noreply.github.com>
2021-01-27 10:19:30 +0530

    Add files via upload

---
+++ main_module_1.py
@@ -0,0 +1,60 @@
+from decrypter_project import decrypt
+from random import randrange
+
+def word():
+    x = open("movie_list.txt","r")
+    text = x.readlines()
+
+    dec = decrypt(text)
+    dec_2 = decrypt(dec)
+
+    ind = randrange(0,len(dec))
+    word = dec_2[ind]
+    return word
+
+    
+print("welcome to hangman.  ............ . enter guess when you think you are ready to guess the word")
+
+def game(word,inp_letter,letters_left,entered_letters,ct): 
+    run = True
+
+    if inp_letter == "guess":
+        guess = input()
+        if guess == word:
+            print('you got the word')
+            run = False
+        else:
+            print("sorry thats not the word")
+            print()
+
+
+    elif len(inp_letter)>1 or inp_letter == "" or inp_letter == " " or not inp_letter.isalpha():
+        print("you entered an invalid input ")
+        print()
+
+
+    elif inp_letter in entered_letters:
+        print("you have aldready entered that letter: ")
+        print()
+
+    else:
+        entered_letters.add(inp_letter)
+        a = word.count(inp_letter)
+        print("letter occours",a,"times")
+        
+        if a == 0:
+            print("not there noob")
+            ct += 1
+
+        elif a:
+            letters_left -= a
+
+        if letters_left == 0:
+            print("you got the word ")
+            run = False
+    return [run,letters_left,entered_letters,ct]
+
+
+
+
+
