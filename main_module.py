ccde684b4a73097551e2dc42a9d813665d0b6bab
Rohan-J-S <53927306+Rohan-J-S@users.noreply.github.com>
2021-02-04 10:00:07 +0530

    Add files via upload

--- main_module.py
+++ main_module.py
@@ -1,102 +1,55 @@
-from decrypter_project import decrypt
-from time import sleep
-from threading import Thread
+from decryption_test import decrypt
 from random import randrange
 
+def word():
+    x = open("movie_list.txt","r")
+    text = x.readlines()
 
-x = open("movie_list.txt","r")
-text = x.readlines()
+    global dec_2 
+    dec_2 = decrypt(text)
 
-dec = decrypt(text)
-dec_2 = decrypt(dec)
+    ind = randrange(0,len(dec_2))
+    word = dec_2[ind]
+    return word
 
-ind = randrange(0,len(dec))
-word = dec_2[ind]
-
-def timer():
-    global ctdn
-    ctdn = 11 #number of seconds +1
-    temp = ctdn
-    for i in range(temp,0,-1):
-        ctdn -= 1
-        #remove once game func added in while
-        sleep(1)
-    exit
     
-
-
 print("welcome to hangman.  ............ . enter guess when you think you are ready to guess the word")
 
+def game(word,inp_letter,letters_left,entered_letters,ct): 
+    run = True
 
-
-def game(word):
-    
-    tries = 7
-    letters_left = 0
-    entered_letters = set()
-   
-
-
-    while tries:
-        
-        inp_letter = input("enter a single letter: ")
-
-        if stop:
-            break
-        
-        if inp_letter == "guess":
-            inp_letter = input("enter the word: ")
-
-            if inp_letter == word:
-                print("you got the word")
-                break
-            else:
-                print("sorry thats not the word")
-                print()
-                continue
-
-        elif len(inp_letter)>1 or inp_letter == "" or inp_letter == " " or not inp_letter.isalpha():
-            print("you entered an invalid input ")
+    if inp_letter == "guess":
+        guess = input()
+        if guess == word:
+            print('you got the word')
+            run = False
+        else:
+            print("sorry thats not the word")
             print()
-            continue
 
-        elif inp_letter in entered_letters:
-            print("you have aldready entered that letter: ")
-            print()
-            continue
 
+    elif len(inp_letter)>1 or inp_letter == "" or inp_letter == " " or not inp_letter.isalpha():
+        print("you entered an invalid input ")
+        print()
+
+
+    elif inp_letter in entered_letters:
+        print("you have aldready entered that letter: ")
+        print()
+
+    else:
         entered_letters.add(inp_letter)
         a = word.count(inp_letter)
         print("letter occours",a,"times")
         
-        if a == False:
-            tries -= 1
-            print(tries, "tries left")
-            print()
+        if a == 0:
+            print("not there noob")
+            ct += 1
 
         elif a:
-            letters_left += a
-            print(len(word)-word.count(" ")-letters_left,"letters left")
-            print()
-            
+            letters_left -= a
 
-        if letters_left == len(word)-word.count(" "):
+        if letters_left == 0:
             print("you got the word ")
-            break
-
-        
-    else:
-        print("out of tries sorry ")
-
-
-stop = False
-th_func = Thread(target = timer)
-game_func = Thread(target = game,args = (word,))
-
-th_func.start()
-game_func.start()
-
-th_func.join()
-stop = True
-
-print("\n time up \n press enter to continue")
+            run = False
+    return [run,letters_left,entered_letters,ct]
